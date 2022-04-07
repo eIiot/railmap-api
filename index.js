c47ea9c8239945cf62f5d02d14e6fee33e9dd493
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-04-07 00:33:17 -0700

    add initial code (bad)

---
+++ index.js
@@ -0,0 +1,40 @@
+import fetch from "node-fetch";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+let caltrainData;
+
+console.log("fetched new data");
+
+fetch(
+  `https://api.511.org/transit/VehicleMonitoring?api_key=${process.env.NEXT_PUBLIC_CALTRAIN_API_KEY}&agency=CT&format=json`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    caltrainData = data;
+  });
+
+caltrainData = new Date();
+
+setInterval(() => {
+  console.log("fetched new data");
+  fetch(
+    `https://api.511.org/transit/VehicleMonitoring?api_key=${process.env.NEXT_PUBLIC_CALTRAIN_API_KEY}&agency=CT&format=json`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      caltrainData = data;
+    });
+}, 60 * 1000);
+
+app.get("/api/caltrain", (req, res) => {
+  res.status(200).json(caltrainData);
+});
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
