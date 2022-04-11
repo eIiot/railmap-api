52bc67b407022a3b475b1d03f0ad834c7a5ea279
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-04-10 17:14:51 -0700

    fix an error where pm2 would restart the file, causing a rate limit

--- index.js
+++ index.js
@@ -9,33 +9,11 @@ const app = express();
 
 app.use(cors());
 
-let caltrainData;
-let viaRailData;
-let actData;
+let caltrainData = "Starting up, please wait...";
+let viaRailData = "Starting up, please wait...";
+let actData = "Starting up, please wait...";
 
 console.log("fetched new data at " + new Date());
-
-fetch(
-  `https://api.511.org/transit/VehicleMonitoring?api_key=${process.env.NEXT_PUBLIC_CALTRAIN_API_KEY}&agency=CT&format=json`
-)
-  .then((res) => res.json())
-  .then((data) => {
-    caltrainData = data;
-  });
-
-fetch(
-  `https://api.511.org/transit/VehicleMonitoring?api_key=${process.env.NEXT_PUBLIC_CALTRAIN_API_KEY}&agency=AC&format=json`
-)
-  .then((res) => res.json())
-  .then((data) => {
-    actData = data;
-  });
-
-fetch(`https://tsimobile.viarail.ca/data/allData.json`)
-  .then((res) => res.json())
-  .then((data) => {
-    viaRailData = data;
-  });
 
 setInterval(() => {
   console.log("fetched new data at " + new Date());
